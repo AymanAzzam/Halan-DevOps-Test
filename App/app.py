32ab5466b5b5efed2054e06fcd4870fc8b7ad445
Ayman Azzam <aymanazzam63@gmail.com>
2021-02-19 04:07:23 +0200

    add variables to app.py

--- App/app.py
+++ App/app.py
@@ -1,3 +1,5 @@
+import sys
+
 import psycopg2
 from psycopg2 import Error
 
@@ -6,9 +8,16 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 def connect_db():
+    db_name = sys.argv[1]
     try:
         # connect to the database
-        connection = psycopg2.connect(database="halan")
+        connection = None
+        if(len(sys.argv) > 2):
+            db_password = sys.argv[2]
+            db_host = sys.argv[3]
+            connection = psycopg2.connect(database=db_name, password=db_password, host=db_host)
+        else:
+            connection = psycopg2.connect(database=db_name)
         # Print PostgreSQL Server Info
         print("\nPostgresSQL Server info: \n", connection.get_dsn_parameters(),"\n")
         return connection
@@ -52,8 +61,11 @@ def home():
 
     return "Halan Rocks"
 
+
 @app.route('/<ip>')
 def add_ip(ip):
+    print("ip request\n")
+    if(ip == "favicon.ico"):    return ""
     # save the ip on the database
     connection = connect_db()
     if(connection):
@@ -64,8 +76,10 @@ def add_ip(ip):
     else:
         return "There is an error in PostrgrsSQL Connection"
 
+
 @app.route('/allips')
 def get_ips():
+    print("allips request\n")
     # retrieve all ips from the database
     connection = connect_db()
     if(connection):
@@ -77,4 +91,8 @@ def get_ips():
         return "There is an error in PostrgrsSQL Connection"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    if(len(sys.argv) > 4 or len(sys.argv) < 2):
+        print("Unvalid Number of argumnts")
+        print("the arguments are databse_name, databse_password(optional), databse_host(optional)")
+    else:
+        app.run(host='0.0.0.0')
