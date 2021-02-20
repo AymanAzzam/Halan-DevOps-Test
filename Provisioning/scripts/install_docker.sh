581c2e5abf0747bf5df04a2a8808391fb365df82
Ayman Azzam <aymanazzam63@gmail.com>
2021-02-20 07:06:51 +0200

    solving bugs

--- Provisioning/scripts/install_docker.sh
+++ Provisioning/scripts/install_docker.sh
@@ -1,5 +1,5 @@
 
-sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt-get update -y
 
