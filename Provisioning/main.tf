74164848d187cbeaa3e600b26440dd83b721746d
Ayman Azzam <aymanazzam63@gmail.com>
2021-02-20 06:28:37 +0200

    Done Postgres master node

--- Provisioning/main.tf
+++ Provisioning/main.tf
@@ -4,11 +4,27 @@ provider "aws"{
     secret_key = var.secret_key
 }
 
+resource "random_string" "rand" {
+    length = 8
+    special = false
+    upper = false
+}
+
 resource "aws_instance" "API" {
     ami = "ami-0634c0a28904f5af6"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.API_sg.id]
     key_name = var.key_name
+
+    provisioner "file" {
+        source      = "scripts/install_postgres.sh"
+        destination = "/tmp/install_postgres.sh"
+    }
+
+    provisioner "file" {
+        source      = "scripts/install_docker.sh"
+        destination = "/tmp/install_docker.sh"
+    }
 
     provisioner "remote-exec" {
         
@@ -20,11 +36,13 @@ resource "aws_instance" "API" {
         }
 
         inline = [
-            "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable'",
-            "sudo apt-get update -y",
-            "sudo apt-get install docker.io",
+            "chmod +x /tmp/install_postgres.sh",
+            "/tmp/install_postgres.sh",
+            "chmod +x /tmp/install_docker.sh",
+            "/tmp/install_docker.sh",
+            "PGPASSWORD=${var.db_password} psql -h ${aws_db_instance.Postgres-Master.endpoint} -U ${var.db_user} -d ${var.db_name} -c 'CREATE TABLE ips ( ip varchar(80));'",
             "docker pull aymanazzam/halantest:halan",
-            "docker run -p ${var.public_port}:5000 aymanazzam/halantest:halan python app.py ${var.db_name} ${var.db_user} ${var.db_password} ${var.db_host} &"
+            "docker run -p ${var.public_port}:5000 aymanazzam/halantest:halan python app.py ${var.db_name} ${var.db_user} ${var.db_password} ${aws_db_instance.Postgres-Master.endpoint} &"
         ]
     }
 }
@@ -51,4 +69,16 @@ resource "aws_security_group" "API_sg" {
 
     }
 
+}
+
+resource "aws_db_instance" "Postgres-Master" {
+    identifier = "postgres-master-${random_string.rand.result}"
+    name = "halan"
+    engine = "postgres"
+    engine_version = "12.5"
+    instance_class = "db.t2.micro"
+    allocated_storage = 20
+    skip_final_snapshot  = true
+    username = var.db_user
+    password = var.db_password
 }
